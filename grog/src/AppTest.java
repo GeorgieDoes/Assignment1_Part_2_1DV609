1682a1801d75cfeb4f28b133ddd83a1d58cf7dfe
JoJo (GeorgieDoes) <gkiliarakis@gmail.com>
2025-11-19 17:31:31 +0100

    RED

--- grog/src/AppTest.java
+++ grog/src/AppTest.java
@@ -9,12 +9,19 @@ public class AppTest {
 
     @BeforeAll
     private void setup() {
-        
+
     }
 
     @Test
     public void TestAppObjectCreation() {
         App app = new App(1, 2);
         assertNotNull("Object should be non empty", app);
+    }
+
+    @Test
+    public void ThestObjectPropertiesSet() {
+        App app = new App(18, 0);
+        assertEquals(18, app.getAge());
+        assertEquals(0, app.getFriends());
     }
 }
