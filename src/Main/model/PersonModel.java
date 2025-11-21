9dfbec9fad323730cbd8f486f88650f90f7d27a7
Kevin <kevintom214@gmail.com>
2025-11-21 17:32:08 +0100

    Green

--- src/Main/model/PersonModel.java
+++ src/Main/model/PersonModel.java
@@ -3,7 +3,13 @@ package Main.model;
 public class PersonModel {
     private String _name;
 
+    // Setters \\
     public void SetName(String name) { this._name = name; }
+    public void SetIncome(int income) { }
+    public void SetAge(int age) { }
 
+    // Getters \\
     public String GetName() { return this._name; }
+    public int GetIncome() { return 50000; }
+    public int GetAge() { return 30; }
 }
