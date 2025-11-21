221a44b6d95b3d81c3969861107f0f35c2accbb3
Kevin <kevintom214@gmail.com>
2025-11-21 17:17:30 +0100

    RED

--- src/Tests/PersonModelTest.java
+++ src/Tests/PersonModelTest.java
@@ -1,13 +1,27 @@
 package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import Main.model.PersonModel;
 
 public class PersonModelTest {
+    private PersonModel person;
+
+    @BeforeEach
+    public void setUp() {
+        person = new PersonModel();
+    }
+    
     @Test
     public void testPersonModelExists() {
-        PersonModel person = new PersonModel();
         assertNotNull(person);
+    }
+
+    @Test
+    public void testPersonModelNameProperty() {
+        person.SetName("Alice");
+        assertEquals("Alice", person.GetName());
     }
 }
