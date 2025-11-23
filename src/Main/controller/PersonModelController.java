f8cf90b1118c9b3cd904eb6c3ef4fdf1b1e6e4c7
Acrobaticbag3 <kevintom214@gmail.com>
2025-11-23 16:03:29 +0100

    REFACTOR

--- src/Main/controller/PersonModelController.java
+++ src/Main/controller/PersonModelController.java
@@ -2,17 +2,20 @@ package Main.controller;
 import Main.model.PersonModel;
 
 public class PersonModelController {
-    private String _name;
-    private int _income;
-    private int _age;
+    private PersonModel _model;
 
     // Setters \\
-    public void setName(String name) { this._name = name; }
-    public void setIncome(int income) { this._income = income; }
-    public void setAge(int age) { this._age = age; }
+    public void setName(String name) { this._model.setName(name); }
+    public void setIncome(int income) { this._model.setIncome(income); }
+    public void setAge(int age) { this._model.setAge(age); }
 
     // Getters \\
-    public String getName() { return this._name; }
-    public int getIncome() { return this._income; }
-    public int getAge() { return this._age; }
+    public String getName() { return this._model.getName(); }
+    public int getIncome() { return this._model.getIncome(); }
+    public int getAge() { return this._model.getAge(); }
+
+    // Constructor \\
+    public PersonModelController() {
+        this._model = new PersonModel();
+    }
 }
