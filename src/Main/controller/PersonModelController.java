97a2f66198a0414084ec685d40c7da5de57a7659
Acrobaticbag3 <kevintom214@gmail.com>
2025-11-23 15:31:04 +0100

    GREEN

---
+++ src/Main/controller/PersonModelController.java
@@ -0,0 +1,6 @@
+package Main.controller;
+import Main.model.PersonModel;
+
+public class PersonModelController {
+    
+}
--- src/Tests/PersonModelControllerTest.java
+++ src/Tests/PersonModelControllerTest.java
@@ -3,8 +3,18 @@ package Tests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import Main.model.PersonModel;
+import Main.controller.PersonModelController;
 
 public class PersonModelControllerTest {
-    
+    private PersonModelController controller;
+
+    @BeforeEach
+    public void setUp() {
+        controller = new PersonModelController();
+    }
+
+    @Test
+    public void testControllerCreation() {
+        assertNotNull(controller);
+    }
 }
