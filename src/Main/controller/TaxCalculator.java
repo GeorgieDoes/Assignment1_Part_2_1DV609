b40fbb1432b9cabcc3f7257ec2bdfa9fe6b274a1
archpc <gkiliarakis@gmail.com>
2025-11-28 14:54:01 +0100

    GREEN Implement calculateTaxes method in TaxCalculator and add initial test cases

--- src/Main/controller/TaxCalculator.java
+++ src/Main/controller/TaxCalculator.java
@@ -3,5 +3,7 @@ package Main.controller;
 import Main.controller.PersonModelController;
 
 public class TaxCalculator {
-    
+    public void calculateTaxes() {
+        // TODO: Implement this method
+    }
 }
