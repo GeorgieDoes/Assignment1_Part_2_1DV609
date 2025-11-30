588bd6ec073edcbaf9f0b733ded49fb83391c127
archpc <gkiliarakis@gmail.com>
2025-11-30 18:46:17 +0100

    REFACTOR TaxCalculator to use constants for tax limits and rates

--- src/Main/controller/TaxCalculator.java
+++ src/Main/controller/TaxCalculator.java
@@ -3,21 +3,26 @@ package Main.controller;
 import Main.controller.PersonModelController;
 
 public class TaxCalculator {
+    private static final int TAX_FREE_LIMIT = 20000;
+    private static final int STATE_TAX_LIMIT = 600000;
+    private static final double MUNICIPAL_TAX_RATE = 0.30;
+    private static final double STATE_TAX_RATE = 0.20;
+
     public double calculateTaxes(PersonModelController person) {
         int income = person.getIncome();
         double tax = 0.0;
 
-        if (income < 20000) {
-            tax = 0.0;
-        } else {
-            tax = (income - 20000) * 0.30;
-            
-            if (income > 600000) {
-                tax += (income - 600000) * 0.20;
-            }
+        if (income < TAX_FREE_LIMIT) {
+            return 0.0;
         }
-        
 
+        // Municipal Tax
+        tax = (income - TAX_FREE_LIMIT) * MUNICIPAL_TAX_RATE;
+        
+        // State Tax
+        if (income > STATE_TAX_LIMIT) {
+            tax += (income - STATE_TAX_LIMIT) * STATE_TAX_RATE;
+        }
 
         return tax;
     }
